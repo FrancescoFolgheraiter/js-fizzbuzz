6d0fda9d84d6760ed0796a9c1a3e0b8febfa7249
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2023-12-05 14:03:01 +0100

    creato ciclo per contare da 1 a 100

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,14 @@ Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un ele
 BONUS 2:
 Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+
+Punti
+1)Scrivi un programma che stampi in console i numeri da 1 a 100
+    -multipli di 3 stampi "Fizz" al posto del numero
+    -multipli di 5 stampi "Buzz"
+    -multipli di 3 che di 5 stampi "FizzBuzz"
 */
+
+for(let i = 0; i < 100 ; i++){
+    console.log(i + 1);
+}
